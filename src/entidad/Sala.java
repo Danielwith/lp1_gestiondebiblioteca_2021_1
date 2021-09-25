6932373c1fa42a801631feb6d07b8b2f5a19a5a0
Fabrizio-ortiz <fabrizioortizgarcia@gmail.com>
2021-09-25 17:12:26 -0500

    enviar CL1 entidad sala

--- src/entidad/Sala.java
+++ src/entidad/Sala.java
@@ -1,5 +1,57 @@
 package entidad;
 
-public class Sala {
+import java.sql.Date;
 
+public class Sala {
+      public int idSala;
+      public String numero;
+      public int piso;
+      public String capacidad;
+      public String recursos;
+      public int estado;
+      public Date fechaRegistro;
+	public int getIdSala() {
+		return idSala;
+	}
+	public void setIdSala(int idSala) {
+		this.idSala = idSala;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public int getPiso() {
+		return piso;
+	}
+	public void setPiso(int piso) {
+		this.piso = piso;
+	}
+	public String getCapacidad() {
+		return capacidad;
+	}
+	public void setCapacidad(String capacidad) {
+		this.capacidad = capacidad;
+	}
+	public String getRecursos() {
+		return recursos;
+	}
+	public void setRecursos(String recursos) {
+		this.recursos = recursos;
+	}
+	public int getEstado() {
+		return estado;
+	}
+	public void setEstado(int estado) {
+		this.estado = estado;
+	}
+	public Date getFechaRegistro() {
+		return fechaRegistro;
+	}
+	public void setFechaRegistro(Date fechaRegistro) {
+		this.fechaRegistro = fechaRegistro;
+	}
+      
+      
 }
