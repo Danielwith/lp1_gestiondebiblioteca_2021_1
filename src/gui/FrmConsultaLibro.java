49056bf2fc1d239f72d7a501db22cc8f823d95b2
Danielwith <daniel.orosco.sc2@hotmail.com>
2021-11-21 22:20:59 -0500

    CL3 - Aron Daniel Orosco Garay - Reporte y Consulta de Libro

--- src/gui/FrmConsultaLibro.java
+++ src/gui/FrmConsultaLibro.java
@@ -1,11 +1,30 @@
 package gui;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.List;
+
 import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+
+import entidad.Libro;
+import model.LibroModel;
 
 public class FrmConsultaLibro extends JInternalFrame {
 
 	private static final long serialVersionUID = 1L;
+	private JTextField txtNombre;
+	private JTable table;
 
 	public FrmConsultaLibro() {
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
@@ -14,8 +33,67 @@ public class FrmConsultaLibro extends JInternalFrame {
 		setClosable(true);
 		setTitle("Consulta Libro");
 		setBounds(100, 100, 1000, 550);
-
-
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Consulta de Libro por Nombre");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 35));
+		lblNewLabel.setBounds(10, 32, 968, 49);
+		getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Nombre:");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel_1.setBounds(62, 120, 127, 25);
+		getContentPane().add(lblNewLabel_1);
+		
+		txtNombre = new JTextField();
+		txtNombre.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				do_txtNombre_keyReleased(e);
+			}
+		});
+		txtNombre.setBounds(160, 120, 653, 26);
+		getContentPane().add(txtNombre);
+		txtNombre.setColumns(10);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(34, 169, 919, 324);
+		getContentPane().add(scrollPane);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"ID", "Titulo", "A\u00F1o", "Categor\u00EDa", "Serie", "Fecha de Registro", "Pa\u00EDs"
+			}
+		));
+		
+		table.setRowSelectionAllowed(true);
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		scrollPane.setViewportView(table);
+		table.getTableHeader().setReorderingAllowed(false);
+		table.setSelectionBackground(Color.GREEN);
+		DefaultTableCellRenderer rightRenderer = new DefaultTableCellRenderer();
+		rightRenderer.setHorizontalAlignment(JLabel.CENTER);
+		table.getColumnModel().getColumn(2).setCellRenderer(rightRenderer);
+		table.getColumnModel().getColumn(5).setCellRenderer(rightRenderer);
+		table.getColumnModel().getColumn(5).setPreferredWidth(100);
+		scrollPane.setViewportView(table);
 	}
-
+	protected void do_txtNombre_keyReleased(KeyEvent e) {
+		String texto = txtNombre.getText();
+		
+		DefaultTableModel dtm = (DefaultTableModel) table.getModel();
+		dtm.setRowCount(0);
+		
+		LibroModel model = new LibroModel();
+		List<Libro> lista = model.listarLibrosPorNombre(texto);
+		
+		for (Libro x: lista) {
+			Object [] fila = {x.getIdLibro(),x.getTitulo(),x.getAnio(),x.getCategoria(),x.getSerie(),x.getFechaRegistro(),x.getPais()};
+			dtm.addRow(fila);
+		}
+	}
 }
